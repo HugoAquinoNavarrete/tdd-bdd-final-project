9ea8b51aba5c266d80606fd94eb018ea3bb8821b
Hugo Aquino <hugo.aquino.navarrete@gmail.com>
2025-04-06 17:00:37 -0400

    Continue with BDD

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -38,7 +38,7 @@ def step_impl(context):
     """ Make a call to the base URL """
     context.driver.get(context.base_url)
     # Uncomment next line to take a screenshot of the web page
-    # context.driver.save_screenshot('home_page.png')
+    context.driver.save_screenshot('home_page.png')
 
 @then('I should see "{message}" in the title')
 def step_impl(context, message):
@@ -105,6 +105,21 @@ def step_impl(context, element_name):
 ##################################################################
 
 ## UPDATE CODE HERE ##
+
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert(found)
 
 ##################################################################
 # This code works because of the following naming convention:
